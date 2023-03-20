372e66097306df4dbd66443e2907f9614c98d707
sqeejee <98850954+sqeejee@users.noreply.github.com>
2023-03-19 17:45:28 -0700

    upload. duh

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+
+const path = require('path');
+
+// Add this line to serve your static files (index.html, CSS, and JS files)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/api/generate-response/:question', async (req, res) => {
+    const { question } = req.params;
+  
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/engines/curie/completions',
+        {
+          prompt: `The user asked: ${question}\nChatGPT's response: `,
+          max_tokens: 250,
+          n: 1,
+          stop: ["\n"],
+          temperature: 1,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+        },
+      );
+  
+      const answer = response.data.choices[0].text.trim();
+      res.status(200).json({ answer });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to generate response.' });
+    }
+  });
+  
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

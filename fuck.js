51a79b8516238c0120f0c81b23b6f951bb66b202
Sam School <school@Sams-MBP.lan>
2023-03-20 22:07:43 -0700

    haha

---
+++ fuck.js
@@ -0,0 +1,14 @@
+//this file only exists to make it appear as js on github. This code does nothing
+
+let johnson = "";
+johnson = "my balls are fat"
+console.log("johnson")
+
+
+let balls = "penis"
+
+for (let i = 0; i < 100000; i++)
+{
+    console.log(balls);
+    console.log("balls;");
+}
